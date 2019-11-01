1d4de1b58d009dd25860464548a0f40ed37d874b
Gerard346 <gerard-ho@hotmail.com>
2019-11-01 19:47:37 +0100

    Pursue

--- Project Artificial/Assets/Scripts/Steering/SteeringPursue.cs
+++ Project Artificial/Assets/Scripts/Steering/SteeringPursue.cs
@@ -20,7 +20,7 @@ public class SteeringPursue : CombineBehaviours
 	// Update is called once per frame
 	void Update () 
 	{
-		//Steer(move.target.transform.position, move.target.GetComponent<Move>().movement, move.target.GetComponent<Move>().max_mov_speed);
+		Steer(move.entity_target.transform.position, move.mov_velocity, move.max_mov_velocity);
 	}
 
 	public void Steer(Vector3 target, Vector3 target_velocity, float max_target_speed)
@@ -33,6 +33,10 @@ public class SteeringPursue : CombineBehaviours
 
         // TODO 6: Improve the prediction based on the distance from
         // our target and the speed we have
+        Vector3 predicted_pos = new Vector3(target.x + target_velocity.x * max_seconds_prediction, target.y + target_velocity.y * max_seconds_prediction, target.z + target_velocity.z * max_seconds_prediction);
+
+        arrive.Steer(predicted_pos);
+        seek.Steer(predicted_pos);
 
     }
 }
