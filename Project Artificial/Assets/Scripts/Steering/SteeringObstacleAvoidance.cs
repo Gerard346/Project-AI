ebefd96fd10764df79597d1982adb6e9d71b69e7
manavld <mlakhwani97@gmail.com>
2019-11-01 22:50:13 +0100

    Steering Obstacle Avoidance and Separation

---
+++ Project Artificial/Assets/Scripts/Steering/SteeringObstacleAvoidance.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SteeringObstacleAvoidance : CombineBehaviours
+{
+
+    public LayerMask mask;
+    public float avoid_distance = 5.0f;
+
+    [System.Serializable]
+    public class Rays
+    {
+        public Vector3 direction;
+        public float length;
+    }
+    public Rays[] rays;
+    private RaycastHit hit1;
+
+    Move move;
+    SteeringSeek seek;
+
+    // Use this for initialization
+    void Start()
+    {
+        move = GetComponent<Move>();
+        seek = GetComponent<SteeringSeek>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Mathf.Atan2(move.mov_velocity.x, move.mov_velocity.z);
+        Quaternion q = Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.up);
+
+        foreach (Rays ray in rays)
+        {
+            Vector3 dirRay = q * ray.direction;
+
+            if (Physics.Raycast(transform.position, dirRay, out hit1, ray.length, mask))
+            {
+                Vector3 escapeTargetPosition = hit1.point + hit1.normal * avoid_distance;
+                if (!move)
+                    move = GetComponent<Move>();
+
+                Vector3 diff = escapeTargetPosition - transform.position;
+                diff.Normalize();
+
+                move.mov_velocity += diff;
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (move && this.isActiveAndEnabled)
+        {
+            Gizmos.color = Color.red;
+            float angle = Mathf.Atan2(move.mov_velocity.x, move.mov_velocity.z);
+            Quaternion q = Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.up);
+
+            foreach (Rays ray in rays)
+            {
+                Vector3 dirRay = q * ray.direction;
+
+                Debug.DrawRay(transform.position, dirRay * ray.length, Color.red);
+            }
+        }
+    }
+}
---
+++ Project Artificial/Assets/Scripts/Steering/SteeringSeparation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringSeparation : CombineBehaviours
+{
+
+    public LayerMask mask;
+    public float search_radius = 5.0f;
+    public AnimationCurve strength;
+
+    Move move;
+
+    // Use this for initialization
+    void Start()
+    {
+        move = GetComponent<Move>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Collider[] tanks = Physics.OverlapSphere(transform.position, search_radius, mask.value);
+        Vector3 separation = Vector3.zero;
+
+        foreach (Collider col in tanks)
+        {
+            if (transform.GetComponent<Collider>() != col)
+            {
+                separation = transform.position - col.transform.position;
+                separation.Normalize();
+                separation *= 0.1f - strength.Evaluate(separation.magnitude / search_radius);
+            }
+            move.mov_velocity += separation;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, search_radius);
+    }
+}
