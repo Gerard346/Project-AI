a7405a3394d2293c1304a1bbc2e6071d0a506ab2
Gerard346 <gerard-ho@hotmail.com>
2019-11-02 14:05:48 +0100

    Camera script for movement

---
+++ Project Artificial/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    Vector3 initial_pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initial_pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (transform.position.x < (initial_pos.x + 15))
+            {
+                transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (transform.position.z < (initial_pos.z + 10))
+            {
+                transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (transform.position.z > (initial_pos.z - 40))
+            {
+                transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (transform.position.x > (initial_pos.x - 10))
+            {
+                transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.position = initial_pos;
+        }
+    }
+}
