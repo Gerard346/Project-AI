8cb0eb34faf168e4e80220688a62fd57ec4183a3
manavld <mlakhwani97@gmail.com>
2019-11-03 00:04:58 +0100

    Trigger for shopkeeper and animation trigger done

---
+++ Project Artificial/Assets/Scripts/Trigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    Animator c_animator;
+
+    void Start()
+    {
+        c_animator = gameObject.GetComponentInParent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    { 
+        if (other.gameObject.CompareTag("Player"))
+            c_animator.SetTrigger("CustomerClose");
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            c_animator.SetTrigger("CustomerClose");
+    }
+    void OnTriggerExit(Collider other)
+    {
+        //UI coins
+
+    }
+}
