519a4167cebbbd54a3876d22b27ddddf562b33a9
manavld <mlakhwani97@gmail.com>
2019-11-03 02:09:05 +0100

    ShopKeeper all done: added ui and generates random points in ui canvas

--- Project Artificial/Assets/Scripts/Trigger.cs
+++ Project Artificial/Assets/Scripts/Trigger.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Trigger : MonoBehaviour
 {
     Animator c_animator;
+    Canvas ui;
+    Text points;
+    int random;
 
     void Start()
     {
         c_animator = gameObject.GetComponentInParent<Animator>();
+        ui = gameObject.GetComponentInChildren<Canvas>();
+        points = ui.gameObject.GetComponentInChildren<Text>();
+        ui.gameObject.SetActive(false);
     }
 
     void Update()
@@ -17,9 +24,14 @@ public class Trigger : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider other)
-    { 
+    {
         if (other.gameObject.CompareTag("Player"))
+        {
             c_animator.SetTrigger("CustomerClose");
+            ui.gameObject.SetActive(true);
+            random = Random.Range(1, 99);
+            points.text = "+" + random;
+        }
     }
     void OnTriggerStay(Collider other)
     {
@@ -29,6 +41,7 @@ public class Trigger : MonoBehaviour
     void OnTriggerExit(Collider other)
     {
         //UI coins
-
+        if (other.gameObject.CompareTag("Player"))
+            ui.gameObject.SetActive(false);
     }
 }
