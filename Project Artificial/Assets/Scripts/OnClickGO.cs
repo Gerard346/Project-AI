19cc31dd35c37eb2582067b2a817a5bd4ea92a3d
manavld <mlakhwani97@gmail.com>
2019-11-28 21:32:25 +0100

    Cashier and cars can only be created in exact position, max of 2 cashiers

--- Project Artificial/Assets/Scripts/OnClickGO.cs
+++ Project Artificial/Assets/Scripts/OnClickGO.cs
@@ -8,6 +8,8 @@ public class OnClickGO : MonoBehaviour
     public GameObject prefab2;
     Ray ray;
     RaycastHit hit;
+    public static bool cashier1_active = false;
+    public static bool cashier2_active = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,14 +26,32 @@ public class OnClickGO : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-
-                //if(hit.collider.name == "GameController")
-                if (Input.GetMouseButtonDown(0))
+                if ((hit.point.x < 21 && hit.point.x > 17) && (hit.point.y < -1 && hit.point.y > -3) && (hit.point.z < -6.2 && hit.point.z > -7.5) && cashier1_active == false)
                 {
-                    //Instantiate(prefab, new Vector3(hit.point.x, -2, hit.point.z), Quaternion.AngleAxis(180, new Vector3(0, 1, 0)));
-                    Instantiate(prefab, hit.point, Quaternion.AngleAxis(180, new Vector3(0, 1, 0)));
-                    //GameObject obj = Instantiate(prefab, new Vector3(20, -2, 13), Quaternion.AngleAxis(180, new Vector3(0, 1, 0))) as GameObject;
-                    //GameObject obj = Instantiate(prefab, new Vector3(20, -2, -6)), Quaternion.AngleAxis(180, new Vector3(0, 1, 0))) as GameObject;
+                    //TODO
+                    //set active false X 
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Instantiate(prefab, hit.point, Quaternion.AngleAxis(180, new Vector3(0, 1, 0)));
+                        cashier1_active = true;
+                    }
+                }
+                else if ((hit.point.x < 21 && hit.point.x > 17.5) && (hit.point.y < -1 && hit.point.y > -3) && (hit.point.z < 14.3 && hit.point.z > 13.2) && cashier2_active == false)
+                {
+                    //TODO
+                    //set active false X
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Instantiate(prefab, hit.point, Quaternion.AngleAxis(180, new Vector3(0, 1, 0)));
+                        cashier2_active = true;
+                    }
+                }
+                else
+                {
+                    //TODO
+                    //set active true X
                 }
             }
         }
@@ -42,13 +62,22 @@ public class OnClickGO : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
 
-                //if(hit.collider.name == "GameController")
-                if (Input.GetMouseButtonDown(0))
+                if ((hit.point.x < 45) && (hit.point.y < -1 && hit.point.y > -3) && (hit.point.z < 26 && hit.point.z > -45))
                 {
-                    //Instantiate(prefab, new Vector3(hit.point.x, -2, hit.point.z), Quaternion.AngleAxis(180, new Vector3(0, 1, 0)));
-                    Instantiate(prefab2, hit.point, Quaternion.identity);
-                    //GameObject obj = Instantiate(prefab, new Vector3(20, -2, 13), Quaternion.AngleAxis(180, new Vector3(0, 1, 0))) as GameObject;
-                    //GameObject obj = Instantiate(prefab, new Vector3(20, -2, -6)), Quaternion.AngleAxis(180, new Vector3(0, 1, 0))) as GameObject;
+                    //TODO
+                    //set active true X
+                }
+                else
+                {
+                    //TODO
+                    //set active false X
+                    //separation between cars 
+                    //(change prefab)
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Instantiate(prefab2, hit.point, Quaternion.identity);
+                    }
                 }
             }
         }
