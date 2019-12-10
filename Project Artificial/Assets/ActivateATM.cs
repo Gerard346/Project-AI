faa46d2e9d15e4c29df6b950a40bad1d9e827f0a
Gerard346 <gerard-ho@hotmail.com>
2019-12-10 19:00:57 +0100

    Activate ATM button and starting UI

---
+++ Project Artificial/Assets/ActivateATM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateATM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject atm_1;
+    public GameObject atm_2;
+    public GameObject atm_3;
+    public GameObject atm_4;
+
+    public void ActivateMoreATM()
+    {
+        if (!atm_2.activeSelf)
+        {
+            atm_2.SetActive(true);
+            return;
+        }
+        if (atm_2.activeSelf && !atm_3.activeSelf)
+        {
+            atm_3.SetActive(true);
+            return;
+        }
+        if (atm_3.activeSelf)
+        {
+            atm_4.SetActive(true);
+            return;
+        }
+        return;
+    }
+}
