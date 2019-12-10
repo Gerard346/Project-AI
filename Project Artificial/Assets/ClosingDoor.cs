c5537237f1f39382ca9e33bdedd58836174500a2
Gerard346 <gerard-ho@hotmail.com>
2019-12-10 17:18:59 +0100

    Cleaner Opens Door

---
+++ Project Artificial/Assets/ClosingDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosingDoor : MonoBehaviour
+{
+    public float angle;
+    public float speed;
+    public Vector3 direction;
+    public float ret;
+    public Vector3 start_rotation;
+    void Start()
+    {
+        start_rotation = this.transform.rotation.eulerAngles;
+        angle = start_rotation.y;
+    }
+    void Update()
+    {
+        ret = Mathf.Round(transform.eulerAngles.y);
+        Debug.Log(ret.ToString());
+        if(Mathf.Round(transform.eulerAngles.y) != angle)
+        {
+            transform.Rotate(direction * speed);
+        }
+    }
+    void OnTriggersEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Cleaner")
+        {
+            angle = start_rotation.y - 90;
+            direction = Vector3.down;
+        }
+    }
+    void OnTriggersExit(Collider other) {
+        if (other.gameObject.tag == "Cleaner")
+        {
+            angle = start_rotation.y;
+            direction = -Vector3.down;
+        }
+    }
+}
