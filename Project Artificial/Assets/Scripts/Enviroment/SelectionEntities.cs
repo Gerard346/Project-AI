de1c3b8ddac385591b786d660c459e144adb0f72
Gerard346 <gerard-ho@hotmail.com>
2019-12-12 11:20:58 +0100

    Selecting entities on mouse position

---
+++ Project Artificial/Assets/Scripts/Enviroment/SelectionEntities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionEntities : MonoBehaviour
+{
+
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.collider.gameObject;
+            // var gameobject_selected = selection.GetComponent<GameObject>();
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (selection != null)
+                {
+                    Debug.Log(selection.name);
+                }
+            }
+        }
+    }
+}
