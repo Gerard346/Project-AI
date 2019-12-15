13949f62ee1e9240ff1d9867b4784f9fb361d65e
manavld <mlakhwani97@gmail.com>
2019-12-15 05:49:20 +0100

    Main menu and Options menu done

---
+++ Project Artificial/Assets/OptionsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audio_mixer;
+
+    public void SetVolume(float volume)
+    {
+        audio_mixer.SetFloat("mainvolume", volume);
+    }
+
+    public void SetQuality(int i)
+    {
+        QualitySettings.SetQualityLevel(i);
+    }
+
+    public void SetFullscreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+    }
+}
