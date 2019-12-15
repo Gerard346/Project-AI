e53b20fb19fa2f8ae653277e9fb1c5f043b6f470
Gerard346 <gerard-ho@hotmail.com>
2019-12-15 09:55:51 +0100

    Dependent fill circular ui

--- Project Artificial/Assets/Scripts/Actions/Dependent/HelpDependent.cs
+++ Project Artificial/Assets/Scripts/Actions/Dependent/HelpDependent.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using NodeCanvas.Framework;
 public class HelpDependent : ActionTask
 {
     // Start is called before the first frame update
     float timer = 0.0f;
     float attend_time = 10.0f;
+    Image watch_image = null;
+
     DependentController dependent_controller;
     protected override void OnExecute()
     {
@@ -15,15 +18,21 @@ public class HelpDependent : ActionTask
         dependent_controller = agent.GetComponent<DependentController>();
         dependent_controller.GetClient().GetComponent<Blackboard>().SetValue("GettingHelp", true);
         dependent_controller.FixRotation();
+        watch_image = agent.transform.Find("Canvas").Find("watch_image").GetComponent<Image>();
+
     }
     protected override void OnUpdate()
     {
+        float watch_value = timer / attend_time;
+        watch_image.fillAmount = watch_value;
+
         timer += Time.deltaTime;
 
         if (timer > attend_time)
         {
             dependent_controller.FreeRotation();
             dependent_controller.GetClient().client_state = ClientController.CLIENT_STATE.CLIENT_GO_BUY;
+            blackboard.SetValue("Selected", false);
             EndAction(true);
         }
     }
