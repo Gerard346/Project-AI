a8021ddd5e1a7d62294d4671136846ea1afe8cd0
Gerard346 <gerard-ho@hotmail.com>
2019-12-15 12:07:01 +0100

    Win&lose condition

---
+++ Project Artificial/Assets/WinLoseCondition.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class WinLoseCondition : MonoBehaviour
+{
+    public Text lose_game;
+    public Text win_game;
+    public GameObject menu_end;
+    int ClientsUnHappy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClientsUnHappy = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(int.Parse(References.data.day_cycle.current_day.TotalDays.ToString()) > 2)
+        {
+            WinGame();
+        }
+        if(ClientsUnHappy > 4)
+        {
+            LoseGame();
+        }
+    }
+
+    void LoseGame()
+    {
+        menu_end.SetActive(true);
+        lose_game.enabled = true;
+    }
+
+    void WinGame()
+    {
+        menu_end.SetActive(true);
+        win_game.enabled = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
